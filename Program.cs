51ad478b250e2364d85d887937c823db2323bf9f
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-08-25 10:04:56 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Let's make a High/Low Number Guessing game for you all to play!");
+        
+        Random rng = new Random();
+        int randomNumber = rng.Next(0, 10);
+
+
+    }
+}
