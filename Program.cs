8fd4963dde9236940899c435ac105eabe7fea414
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-08-28 16:03:54 -0500

    rearranged items to have better flow of code

--- Program.cs
+++ Program.cs
@@ -8,15 +8,15 @@ class Program
 
         const int MAX_USER_INPUT = 100;
         const int MIN_USER_INPUT = 0;
-       // const int MAX_AMOUNT_OF_TRIES = 4;
+        const int MAX_AMOUNT_OF_TRIES = 4;
         
         Random rng = new Random();
-        int secretNumber = rng.Next(0, 100);
+        int secretNumber = rng.Next(MIN_USER_INPUT, MAX_USER_INPUT);
         Console.Write("What is your guess for the secret number: ");
         int userNumberInput = int.Parse(Console.ReadLine());
         int triesLeft = 5;
         
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < MAX_AMOUNT_OF_TRIES; i++)
         {
             triesLeft = triesLeft - 1;   // why isn't this returning the countdown?
             if (secretNumber == userNumberInput)
@@ -31,29 +31,24 @@ class Program
             } 
             if (userNumberInput < secretNumber)
             {
-                Console.WriteLine($"Too Low! Try again! You have {triesLeft} amount of tries left.");
+                Console.WriteLine("Too Low! Try again!");
                 // if triesLeft equals 0 then stop.
             }
             if (userNumberInput > secretNumber)
             {
-                Console.WriteLine($"Too High! Try again! You have {triesLeft} amount of tries left.");
+                Console.WriteLine("Too High! Try again!");
                 // if triesLeft equals 0 then stop.
             }
-            Console.Write("What is your next guess for the secret number? ");
+            Console.Write($"You have {triesLeft} tries left. What is your next guess for the secret number? ");
             int.Parse(Console.ReadLine());
-               
             
-            // I have noticed for every new guess it is the same output as the prior one before (or the first output).
+            // I have noticed for every new guess the output is as the prior one before (or the first output) ex. "Too High".
             // why is it not rechecking the number against the arguments?
-           
             // When the counter gets down to 0 it needs to stop and restart the game.
-
             // output if they are too high or too low (separately)
             // output if they are within 5 numbers of secret guess
-
-            // Need to put a limit on guesses. Can use return to stop while loop?
             // Look up documentation for Math.Abs vs Range check
-
         }
+        
     }
 }
